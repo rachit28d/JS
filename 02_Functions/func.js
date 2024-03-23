c08430ae29b192164ccfadb471157a3c762e9c43
rachit28d <13940739+rachit28d@users.noreply.github.com>
2024-03-23 06:28:46 +0000

    container config done

---
+++ 02_Functions/func.js
@@ -0,0 +1,9 @@
+// Function expression and anonymous function
+const calcage = function (birthyear){
+    return 2024 - birthyear;
+}
+console.log(calcage(1986));
+
+
+//Arrow Function
+birthyear => 2024 - birthyear;
